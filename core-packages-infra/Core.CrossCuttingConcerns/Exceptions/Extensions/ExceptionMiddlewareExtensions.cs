367ba819c5ea02756b3e5f957526c642b309c8af
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-10-16 13:54:57 +0300

    initial core packages of infrastructer

---
+++ core-packages-infra/Core.CrossCuttingConcerns/Exceptions/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    
+    public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+    {
+        Console.WriteLine("test "+app.ApplicationServices);
+        app.UseMiddleware<ExceptionMiddleware>();
+    }
+        
+}
